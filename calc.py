5c4b33adca6a2c5a1da89ba1686a1de744433522
“kamesh” <kamesh@gmail.com>
2022-02-14 07:18:36 +0000

    3rd commit

--- calc.py
+++ calc.py
@@ -21,14 +21,14 @@ print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
-print("4.Divide")
+print("5.Divide")
 
 while True:
     # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
+    choice = input("Enter choice(1/2/3/5): ")
 
     # check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
+    if choice in ('1', '2', '3', '5'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
@@ -41,7 +41,7 @@ while True:
         elif choice == '3':
             print(num1, "*", num2, "=", multiply(num1, num2))
 
-        elif choice == '4':
+        elif choice == '5':
             print(num1, "/", num2, "=", divide(num1, num2))
         
         # check if user wants another calculation
